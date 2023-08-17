b9ca3968b95b323b954cd5e9cce7c82c16ed2fec
Recep Tayyip Erdogan <receptayyiperdogan8@gmail.com>
2023-08-17 18:44:52 +0300

    Avoid name confusion & Detailed swagger documentation

--- src/main/java/com/demo/filemanager/controller/FileController.java
+++ src/main/java/com/demo/filemanager/controller/FileController.java
@@ -2,8 +2,14 @@ package com.demo.filemanager.controller;
 
 import com.demo.filemanager.dto.FileMetaDataDTO;
 import com.demo.filemanager.model.FileMetaData;
-import com.demo.filemanager.model.response.ApiResponse;
+import com.demo.filemanager.model.response.CustomApiResponse;
 import com.demo.filemanager.service.FileService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -19,7 +25,7 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
-
+@Tag(name = "File", description = "File management APIs")
 @RestController
 @RequestMapping("/api/files")
 @RequiredArgsConstructor
@@ -30,27 +36,50 @@ public class FileController {
     @Value("${file.storage.location}")
     private String fileStorageLocation;
 
-
+    @Operation(
+            summary = "Retrieve all files",
+            description = "Get a list of all File meta data.",
+            tags = { "files" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = FileMetaDataDTO.class), mediaType = "application/json") })
+    })
     @GetMapping
-    public ResponseEntity<ApiResponse<List<FileMetaDataDTO>>> getAllFiles() {
+    public ResponseEntity<CustomApiResponse<List<FileMetaDataDTO>>> getAllFiles() {
         List<FileMetaData> files = fileService.getAllFiles();
         List<FileMetaDataDTO> dtos = files.stream()
                 .map(this::convertToFileMetaDataDTO)
                 .collect(Collectors.toList());
-        return ResponseEntity.ok(new ApiResponse<>("All files fetched successfully.", dtos));
+        return ResponseEntity.ok(new CustomApiResponse<>("All files fetched successfully.", dtos));
     }
 
+    @Operation(
+            summary = "Retrieve a File by Id",
+            description = "Get a File meta data object by specifying its id.",
+            tags = { "files", "get" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = FileMetaDataDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "404", content = { @Content(schema = @Schema()) })
+    })
     @GetMapping("/{id}")
-    public ResponseEntity<ApiResponse<FileMetaDataDTO>> getFileById(@PathVariable Long id) {
+    public ResponseEntity<CustomApiResponse<FileMetaDataDTO>> getFileById(@PathVariable Long id) {
         Optional<FileMetaData> file = fileService.getFileById(id);
-        return file.map(metaData -> ResponseEntity.ok(new ApiResponse<>("File fetched successfully.", convertToFileMetaDataDTO(metaData))))
+        return file.map(metaData -> ResponseEntity.ok(new CustomApiResponse<>("File fetched successfully.", convertToFileMetaDataDTO(metaData))))
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @Operation(
+            summary = "Upload a file",
+            description = "Upload a file and save its meta data.",
+            tags = { "files", "upload" })
+    @ApiResponses({
+            @ApiResponse(responseCode = "200", content = { @Content(schema = @Schema(implementation = FileMetaDataDTO.class), mediaType = "application/json") }),
+            @ApiResponse(responseCode = "400", content = { @Content(schema = @Schema()) }),
+            @ApiResponse(responseCode = "500", content = { @Content(schema = @Schema()) })
+    })
     @PostMapping("/upload")
-    public ResponseEntity<ApiResponse<FileMetaDataDTO>> uploadFile(@RequestParam("file") MultipartFile file) {
+    public ResponseEntity<CustomApiResponse<FileMetaDataDTO>> uploadFile(@RequestParam("file") MultipartFile file) {
         if (!isValidFile(file)) {
-            return ResponseEntity.badRequest().body(new ApiResponse<>("Invalid file type or size!", null));
+            return ResponseEntity.badRequest().body(new CustomApiResponse<>("Invalid file type or size!", null));
         }
 
         String fileName = storeFile(file);
@@ -65,9 +94,31 @@ public class FileController {
 
         FileMetaData savedFile = fileService.saveFile(metaData);
 
-        return ResponseEntity.ok(new ApiResponse<>("File uploaded successfully.", convertToFileMetaDataDTO(savedFile)));
+        return ResponseEntity.ok(new CustomApiResponse<>("File uploaded successfully.", convertToFileMetaDataDTO(savedFile)));
     }
 
+    @Operation(
+            summary = "Download a file by its Id",
+            description = "Retrieve the actual content of a file given its meta data ID. This endpoint will provide the file as a byte array, allowing for direct downloads.",
+            tags = { "files", "download" }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Successful retrieval of file.",
+                    content = @Content(schema = @Schema(type = "string", format = "binary"), mediaType = "*/*")
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "File not found.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error.",
+                    content = @Content
+            )
+    })
     @GetMapping("/download/{id}")
     public ResponseEntity<byte[]> downloadFile(@PathVariable Long id) {
         Optional<FileMetaData> fileMetaDataOptional = fileService.getFileById(id);
@@ -90,14 +141,58 @@ public class FileController {
         }
     }
 
+    @Operation(
+            summary = "Delete a file meta data by its Id",
+            description = "Delete the meta data information of a file given its ID. This will not delete the actual file but only its reference in the system.",
+            tags = { "files", "delete" }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "File meta data deleted successfully.",
+                    content = @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "File not found.",
+                    content = @Content
+            )
+    })
     @DeleteMapping("/{id}")
-    public ResponseEntity<ApiResponse<String>> deleteFile(@PathVariable Long id) {
+    public ResponseEntity<CustomApiResponse<String>> deleteFile(@PathVariable Long id) {
         fileService.deleteFile(id);
-        return ResponseEntity.ok(new ApiResponse<>("File deleted successfully.", null));
+        return ResponseEntity.ok(new CustomApiResponse<>("File deleted successfully.", null));
     }
 
+    @Operation(
+            summary = "Update file and its meta data by its Id",
+            description = "Update the content and meta data of a file given its ID. This requires uploading a new file that will replace the old one.",
+            tags = { "files", "update" }
+    )
+    @ApiResponses({
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "File updated successfully.",
+                    content = @Content(schema = @Schema(implementation = CustomApiResponse.class), mediaType = "application/json")
+            ),
+            @ApiResponse(
+                    responseCode = "400",
+                    description = "Invalid file type or size.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "404",
+                    description = "File not found.",
+                    content = @Content
+            ),
+            @ApiResponse(
+                    responseCode = "500",
+                    description = "Internal server error.",
+                    content = @Content
+            )
+    })
     @PutMapping("/{id}")
-    public ResponseEntity<ApiResponse<FileMetaDataDTO>> updateFile(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException {
+    public ResponseEntity<CustomApiResponse<FileMetaDataDTO>> updateFile(@PathVariable Long id, @RequestParam("file") MultipartFile file) throws IOException {
         Optional<FileMetaData> existingFileMetaData = fileService.getFileById(id);
 
         if (!existingFileMetaData.isPresent()) {
@@ -105,7 +200,7 @@ public class FileController {
         }
 
         if (!isValidFile(file)) {
-            return ResponseEntity.badRequest().body(new ApiResponse<>("Invalid file type or size!", null));
+            return ResponseEntity.badRequest().body(new CustomApiResponse<>("Invalid file type or size!", null));
         }
 
         FileMetaData updatedMetaData = existingFileMetaData.get();
@@ -118,7 +213,7 @@ public class FileController {
 
         FileMetaData savedMetaData = fileService.saveFile(updatedMetaData);
 
-        return ResponseEntity.ok(new ApiResponse<>("File updated successfully.", convertToFileMetaDataDTO(savedMetaData)));
+        return ResponseEntity.ok(new CustomApiResponse<>("File updated successfully.", convertToFileMetaDataDTO(savedMetaData)));
     }
 
     private boolean isValidFile(MultipartFile file) {
