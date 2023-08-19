58134aa68a5f5fe054c89946450b41f263ce3edd
Recep Tayyip Erdogan <receptayyiperdogan8@gmail.com>
2023-08-19 18:00:49 +0300

    Logger implementation

--- src/main/java/com/demo/filemanager/controller/AuthenticationController.java
+++ src/main/java/com/demo/filemanager/controller/AuthenticationController.java
@@ -4,6 +4,8 @@ import com.demo.filemanager.dao.request.SignUpRequest;
 import com.demo.filemanager.dao.request.SigninRequest;
 import com.demo.filemanager.dao.response.JwtAuthenticationResponse;
 import com.demo.filemanager.service.AuthenticationService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -16,13 +18,17 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class AuthenticationController {
     private final AuthenticationService authenticationService;
+    private static final Logger logger = LoggerFactory.getLogger(AuthenticationController.class);
     @PostMapping("/signup")
     public ResponseEntity<JwtAuthenticationResponse> signup(@RequestBody SignUpRequest request) {
+        logger.info("Signed up");
         return ResponseEntity.ok(authenticationService.signup(request));
     }
 
-    @PostMapping("/signin")
+    @PostMapping("/signing")
     public ResponseEntity<JwtAuthenticationResponse> signin(@RequestBody SigninRequest request) {
+        logger.info("Signing");
+
         return ResponseEntity.ok(authenticationService.signin(request));
     }
 }
--- src/main/java/com/demo/filemanager/controller/FileController.java
+++ src/main/java/com/demo/filemanager/controller/FileController.java
@@ -11,6 +11,8 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -35,6 +37,9 @@ public class FileController {
 
     @Value("${file.storage.location}")
     private String fileStorageLocation;
+
+    private static final Logger logger = LoggerFactory.getLogger(FileController.class);
+
 
     @Operation(
             summary = "Retrieve all files",
